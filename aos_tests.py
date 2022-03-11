d1600464d4255bc28b1a61cdbc26e349dbf6ae86
lilyliu2021 <lily.liu201209@gmail.com>
2022-03-11 00:05:26 -0800

    running problems fixed

--- aos_tests.py
+++ aos_tests.py
@@ -7,6 +7,7 @@ class AOSAppPositiveTestCases(unittest.TestCase):  #create class
     @staticmethod              #signals to unittest that this is a static method
     def test_create_new_user():
         methods.setUp()
+        methods.check_homepage()
         methods.create_new_account()
         methods.validate_new_account()
         print(f'------New account is created, Username is {locators.new_user_name}')
@@ -25,15 +26,17 @@ class AOSAppPositiveTestCases(unittest.TestCase):  #create class
         # sleep(0.25)
         methods.tearDown()
 
-    def test_homepage(self):
-        methods.setUp()
-        methods.check_homepage_text()
-        methods.check_shopnow_button()
-        methods.check_main_menu()
-        methods.check_mainlogo()
-        methods.contact_us()
-        methods.check_socialmedia_link()
-        methods.tearDown()
+    # def test_homepage(self):
+    #     methods.setUp()
+    #     methods.check_homepage_text()
+    #     methods.check_shopnow_button()
+    #     methods.check_main_menu()
+    #     methods.check_mainlogo()
+    #     methods.contact_us()
+    #     methods.check_socialmedia_link()
+    #     methods.tearDown()
+
+#unittest.main()
 
 # setUp()
 # # Create New Account
