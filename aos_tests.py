b0f655875e96aa4dd0e72485d7f121b581ecb420
lilyliu2021 <lily.liu201209@gmail.com>
2022-03-12 17:47:46 -0800

    Added Checkout

--- aos_tests.py
+++ aos_tests.py
@@ -5,27 +5,27 @@ import aos_methods as methods
 class AOSAppPositiveTestCases(unittest.TestCase):  #create class
 
     @staticmethod              #signals to unittest that this is a static method
-    def test_create_new_user():
+    #def test_create_new_user():
+    def test_aos():
         methods.setUp()
         methods.check_homepage()
         methods.create_new_account()
         methods.validate_new_account()
         print(f'------New account is created, Username is {locators.new_user_name}')
-        # Logout
         methods.logout()
-        #sleep(0.5)
-        # Login
         methods.login()
         # Validate New User can login (see if you can reuse New Account Validation)
         methods.validate_new_account()
-        print(f'------New user {locators.new_user_name} can log in!')
+        #print(f'------New user {locators.new_user_name} can log in!')
         methods.logger('created')
+        methods.checkout()
         # Logout
         methods.logout()
-        print(f'------New user {locators.new_user_name} can log out successfully!')
-        # sleep(0.25)
+        #print(f'------New user {locators.new_user_name} can log out successfully!')
         methods.login()
         methods.validate_new_account()
+        methods.view_cart()
+        methods.cancel_order()
         methods.delete_account()
         methods.tearDown()
 
@@ -37,9 +37,7 @@ class AOSAppPositiveTestCases(unittest.TestCase):  #create class
     #     methods.check_mainlogo()
     #     methods.contact_us()
     #     methods.check_socialmedia_link()
-    #     methods.tearDown()
-
-#unittest.main()
+    #     methods.tearDown
 
 # setUp()
 # # Create New Account
